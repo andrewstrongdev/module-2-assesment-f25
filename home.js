7280f7f9894cfb73f28770112afee580111c52d7
Andrew Strong <andrewstrongdev@gmail.com>
2022-06-25 13:50:56 -0600

    home.js problems done. Took about 10 minutes of work and 20 minutes of stairing out the window

--- home.js
+++ home.js
@@ -23,7 +23,7 @@
 
 //CODE HERE
 const greetUser = (username) => `Welcome back, ${username}`
-console.log(greetUser('Andrew'))
+//console.log(greetUser('Andrew'))
 
 
 
@@ -53,6 +53,15 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
 //CODE HERE
 
+const canWeDeliver = (zipCode) => {
+  if(deliveryAreaZipCodes.includes(zipCode)){
+    return "You're in the delivery zone!"
+  } else {
+    return "Sorry, we can't deliver to that address"
+  }
+}
+//console.log(canWeDeliver(85201))
+
 
 
 /* 
@@ -73,6 +82,14 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 */
 
 // CODE HERE
+const canWeDeliverTwo = (zipCode) => {
+    for(let i =0; i<deliveryAreaZipCodes.length; i++){
+        if(zipCode === deliveryAreaZipCodes[i]){
+            return "You're in the delivery zone!"
+        }
+    } return "Sorry, we can't deliver to that address"
+}
+//console.log(canWeDeliverTwo(85204))
 
 
 //////////////////PROBLEM 3////////////////////
@@ -109,7 +126,8 @@ const deals = [
 */
 
 //CODE HERE
-
+deals[0].title = deals[0].title.replace(15,10)
+console.log(deals)
 
 
 /*
@@ -126,3 +144,5 @@ const deals = [
 */
 
 //CODE HERE
+deals[1].desc = deals[1].desc.replace('March', 'April').trim()
+console.log(deals)
